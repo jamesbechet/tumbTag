c8c7075ce462f04982a8f4a9eb91f8c71c5637f3
jboulouloubi <jamesbechet@gmail.com>
2013-11-27 14:26:10 +0100

    Added the tags’ list input

--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
 var tumbTag = {
-  listTags: null,
+  tagsList: null,
+  menuElem: document.getElementById('menu'),
   tagElem: document.getElementById('add-tags'),
   listElem: document.getElementById('add-list'),
+  tagsListElem: document.getElementById('tags-list'),
+  validateTagsElem: document.getElementById('validate-tags'),
 
   // Init the object
   init: function () {
-    this.listTags = this.retrieveList();
-    if (this.listTags && this.listTags.length) {
+    this.tagsList = this.retrieveList();
+    if (this.tagsList && this.tagsList.length) {
       this.tagElem.className += 'list-loaded';
     }
     this.bindEvents();
@@ -14,7 +17,7 @@ var tumbTag = {
 
   // Bind all the events
   bindEvents: function () {
-    this.tagElem.onclick = this.addTags.bind(this);
+    this.tagElem.onclick  = this.addTags.bind(this);
     this.listElem.onclick = this.createList.bind(this);
   },
 
@@ -25,11 +28,14 @@ var tumbTag = {
 
   // Create the tags' list
   createList: function () {
+    this.menuElem.className             = 'hide';
+    this.tagsListElem.className     = 'show';
+    this.validateTagsElem.className = 'show';
   },
 
   // Add the tags in the tumblr's input
   addTags: function () {
-    if (this.listTags && this.listTags.length) {
+    if (this.tagsList && this.tagsList.length) {
       // Can add the tags
     } else {
       // Display buble saying to add a list
