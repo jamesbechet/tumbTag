c7e105f150d8ab26ea7e0dd04b9416ff8bf05d7d
jboulouloubi <jamesbechet@gmail.com>
2013-11-29 00:04:53 +0100

    Seems to work, need to do some tests.

--- content.js
+++ content.js
@@ -1,7 +1,18 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-switch(msg.type) {
-        case "add-tags":
-                console.log(msg);
-        break;
+switch (msg.type) {
+  case "add-tags":
+    var $tagInput = $('input.editor'),
+        tagsNb   = msg.tagsList && msg.tagsList.length;
+
+    if (tagsNb && $tagInput.length) {
+      for (var i = 0; i < tagsNb; i += 1) {
+        if (i === (tagsNb - 1)) { // Last value, different behavior
+          $tagInput.val(msg.tagsList[i]);
+        } else { // Add span
+          $tagInput.before('<span class="tag">' + msg.tagsList[i] + '</span>');
+        }
+      }
+    }
+    break;
 }
 });
