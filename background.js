59bc41a178efd721b5ff7aef0b6565925fc4cb34
jboulouloubi <jamesbechet@gmail.com>
2013-11-29 21:56:36 +0100

    Add the last tag, avoid to do one more click to add it.

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 // listening for an event / one-time requests
 // coming from the popup
 chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
-    switch(req.type) {
-        case "add-tags":
-            addTags(req);
-        break;
-    }
-    return true;
+	switch(req.type) {
+	    case "add-tags":
+	        addTags(req);
+	    break;
+	}
+	return true;
 });
 
 // send a message to the content script
 var addTags = function(req) {
-        chrome.tabs.getSelected(null, function(tab){
-            chrome.tabs.sendMessage(tab.id, {type: req.type, tagsList: req.tagsList});
-        });
+  chrome.tabs.getSelected(null, function(tab){
+      chrome.tabs.sendMessage(tab.id, {type: req.type, tagsList: req.tagsList});
+  });
 }
--- content.js
+++ content.js
@@ -3,12 +3,14 @@ switch (msg.type) {
   case "add-tags":
     var $tagInput     = $('input.editor'),
         $prevTagInput = $('div.editor_wrapper'),
+        $postContent  = $('div#post_content'),
         tagsNb        = msg.tagsList && msg.tagsList.length;
 
     if (tagsNb && $tagInput.length) {
       for (var i = 0; i < tagsNb; i += 1) {
         if (i === (tagsNb - 1)) { // Last value, different behavior
           $tagInput.val(msg.tagsList[i]);
+          $postContent.trigger('click'); // Add the last element, avoid one more click
         } else { // Add span
           $prevTagInput.before('<span class="tag">' + msg.tagsList[i] + '</span>');
         }
