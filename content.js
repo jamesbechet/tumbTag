47cdf8ffe24f99aa3c4eca661237b0b2a934088a
jboulouloubi <jamesbechet@gmail.com>
2013-11-29 13:58:14 +0100

    Adding tag has a better behavior

--- content.js
+++ content.js
@@ -1,15 +1,16 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
 switch (msg.type) {
   case "add-tags":
-    var $tagInput = $('input.editor'),
-        tagsNb   = msg.tagsList && msg.tagsList.length;
+    var $tagInput     = $('input.editor'),
+        $prevTagInput = $('div.editor_wrapper'),
+        tagsNb        = msg.tagsList && msg.tagsList.length;
 
     if (tagsNb && $tagInput.length) {
       for (var i = 0; i < tagsNb; i += 1) {
         if (i === (tagsNb - 1)) { // Last value, different behavior
           $tagInput.val(msg.tagsList[i]);
         } else { // Add span
-          $tagInput.before('<span class="tag">' + msg.tagsList[i] + '</span>');
+          $prevTagInput.before('<span class="tag">' + msg.tagsList[i] + '</span>');
         }
       }
     }
