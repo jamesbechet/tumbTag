95e48d19641f25ece555f1ea4a54a404dd057f35
jboulouloubi <jamesbechet@gmail.com>
2017-03-12 17:19:57 -0400

    :sparkles: cleanup + edit tags

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 $(function() {
+
   var tumbTag = {
+
     tags : [],
 
     $tumbTagSelector : null,
@@ -31,7 +33,7 @@ $(function() {
       'line-height' : '1em'
     },
 
-    $addListSelector : $('<div id="tumbTag-addList"></div>'),
+    $addListSelector : null,
     addListElemCss   : {
       'width'            : '200px',
       'height'           : '250px',
@@ -57,7 +59,7 @@ $(function() {
     $addListButtonSelector : null,
     addListButtonElem      : '<button id="tumbTag-addListButton">Done</button>',
     addListButtonElemCss   : {
-      'padding'          : '.5em 1em',
+      'padding'          : '.2em 1em',
       'display'          : 'block',
       'margin'           : '0 auto',
       'background-color' : '#529ecc'
@@ -86,18 +88,37 @@ $(function() {
       'line-height'  : '1em'
     },
 
-    init: function () {
+
+
+    /**
+     * The list being edited.
+     * @type {Object}
+     *       {String} name
+     *       {Array} list
+     */
+    editingList : null,
+
+
+
+    init : function () {
+
       var that          = this,
-          getStoredTags = window.localStorage.tags ? this.syncStorages.bind(this) : this.getSavedTags.bind(this)
+          getStoredTags = window.localStorage.tags
+            ? this.syncStorages.bind(this)
+            : this.getSavedTags.bind(this)
 
       getStoredTags().
         then(function (tags) {
           that.tags = tags
           that.periodicallyCheckIfRender()
         })
+
     },
 
-    appendTags: function ($selector) {
+
+
+    appendTags : function ($selector) {
+
       var $shouldAppendTo = $('.tag-input-wrapper .editor-plaintext'),
           tagObj          = this.tags[$selector.index()]
 
@@ -111,9 +132,13 @@ $(function() {
       setTimeout(function () {
         $('.tag-input-wrapper .editor-placeholder').text('')
       }, 0)
+
     },
 
-    removeList: function ($selector) {
+
+
+    removeList : function ($selector) {
+
       this.tags = _.reject(this.tags, function (tagObj) {
         return tagObj.name === $selector.parent().text()
       })
@@ -121,37 +146,64 @@ $(function() {
       chrome.storage.sync.set({ 'tags' : this.tags })
       $selector.parent().remove()
       this.adjustPosition()
+
     },
 
-    bindNewTagListsEvent: function () {
-      var that = this
 
-      $('.tumbTag-list').last().click(function (e) {
-        that.appendTags($(e.currentTarget))
-        return false
-      })
 
-      $('.icon_close').last().click(function (e) {
-        that.removeList($(e.currentTarget))
-        return false
-      })
+    bindNewTagListsEvent : function () {
+      $('.tumbTag-list').last().click(this.onClickTagList.bind(this))
+      $('.icon_close').last().click(this.onClickRemoveList.bind(this))
     },
 
-    bindTagListsEvent: function () {
-      var that = this
 
-      $('.tumbTag-list').click(function (e) {
-        that.appendTags($(e.currentTarget))
-        return false
-      })
 
-      $('.icon_close').click(function (e) {
-        that.removeList($(e.currentTarget))
-        return false
-      })
+    bindTagListsEvent : function () {
+      $('.tumbTag-list').click(this.onClickTagList.bind(this))
+      $('.icon_close').click(this.onClickRemoveList.bind(this))
     },
 
-    adjustPosition: function () {
+
+
+    onClickTagList : function (event) {
+
+      // If the add list selector is open, edit the list
+      if (this.isEditorVisible()) {
+        this.editTagList($(event.currentTarget))
+      }
+      //  If the add list selector is closed, append the tags
+      else {
+        this.appendTags($(event.currentTarget))
+      }
+
+      return false
+
+    },
+
+
+
+    editTagList : function ($selector) {
+
+      // Get the list being edited
+      var tagList      = this.tags[$selector.index()]
+      this.editingList = tagList
+
+      // Render the list
+      this.renderEditor()
+
+    },
+
+
+
+    onClickRemoveList : function (event) {
+      this.removeList($(event.currentTarget))
+      return false
+    },
+
+
+
+    adjustPosition : function () {
+
       var $postContainer = $('.post-container')
       var postTop        = $postContainer.offset().top
       var postLeft       = $postContainer.offset().left
@@ -166,9 +218,13 @@ $(function() {
       }
       this.$tumbTagSelector.css('top', finalTop.toString() + 'px')
       this.$tumbTagSelector.css('left', (postLeft + postWidth + 200).toString() + 'px')
+
     },
 
-    appendTagLists: function () {
+
+
+    appendTagLists : function () {
+
       var that = this
 
       this.$tumbTagSelector.append(this.tagListsElem)
@@ -182,9 +238,17 @@ $(function() {
       $('.tumbTag-list').css(this.listElemCss)
       $('.icon_close').css(this.iconRemoveElemCss)
       this.bindTagListsEvent()
+
     },
 
-    setListName: function (listName) {
+
+
+    setListName : function (listName) {
+
+      if (this.editingList) {
+        return listName
+      }
+
       var tags         = this.tags,
           loop         = 5,
           existingName = listName
@@ -202,20 +266,44 @@ $(function() {
       }
 
       return listName
+
     },
 
-    storeList: function (name, list) {
+
+
+    addList : function (name, list) {
+
       this.tags.push({
         name : name,
         list : list
       })
 
-      chrome.storage.sync.set({ tags: this.tags })
+      this.save()
 
-      return _.last(this.tags)
     },
 
-    appendNewList: function (newList) {
+
+
+    editList : function (name, list) {
+
+      // NOTE: Mutation is bad
+      this.editingList.name = name
+      this.editingList.list = list
+
+      this.save()
+
+    },
+
+
+
+    save : function () {
+      chrome.storage.sync.set({ tags : this.tags })
+    },
+
+
+
+    appendNewList : function (newList) {
+
       this.$addListSelector.hide('medium')
       this.$tagListsSelector
         .append('<li class="tumbTag-list">' + newList.name + this.iconRemoveElem + '</li>')
@@ -225,41 +313,77 @@ $(function() {
       this.bindNewTagListsEvent()
 
       this.adjustPosition()
+
     },
 
-    bindValidateList: function () {
-      var that = this,
-          newList,
-          listName,
-          tags
 
-      this.$addListButtonSelector.click(function (e) {
-        listName = that.setListName(that.$addListNameSelector.val() || 'test')
-        tags     = that.$addListTagsSelector.val().replace(/\r\n/g, '\n').split('\n')
-        tags     = _.filter(tags, function (tag) { return !!tag })
 
-        newList = that.storeList(listName, tags)
-        that.appendNewList(newList)
-        return false
-      })
+    bindValidateList : function () {
+      this.$addListButtonSelector.click(this.onClickAddListButton.bind(this))
     },
 
-    buildNewList: function () {
+
+
+    onClickAddListButton : function () {
+
+      let listName = this.setListName(this.$addListNameSelector.val())
+      let tags     = this.$addListTagsSelector.val().replace(/\r\n/g, '\n').split('\n')
+      tags         = _.filter(tags, function (tag) { return !!tag })
+
+      if (this.editingList) {
+        this.editList(listName, tags)
+        this.editingList = null
+      }
+      else {
+        this.addList(listName, tags)
+        this.appendNewList(_.last(this.tags))
+      }
+
+      this.toggleAddList()
+
+      return false
+
+    },
+
+
+
+    toggleAddList : function () {
+
+      // If the editor is visible, hide it
+      if (this.isEditorVisible()) {
+        this.hideEditor()
+        return
+      }
+
+      // If the editor doesn't exist, create it
       if (!this.$addListSelector) {
-        this.$addListSelector = $('#tumbTag-addList')
-        return this.$addListSelector.show('medium')
+        this.createEditor()
       }
 
-      if (this.$addListSelector.css('display') === 'block') {
-        return this.$addListSelector.hide('medium')
-      }
+      // If the editor is hidden, show it
+      this.renderEditor()
+      this.showEditor()
 
-      if ($('#tumbTag-addList').length) {
-        return this.$addListSelector.show('medium')
-      }
+    },
 
-      // Container
+
+
+    hideEditor() {
+      this.$addListSelector.hide('medium')
+    },
+
+
+    showEditor() {
+      this.$addListSelector.show('medium')
+    },
+
+
+    createEditor() {
+
+      this.$addListSelector = $('<div id="tumbTag-addList"></div>')
       this.$addListSelector.hide()
+      
+      // Container
       this.$iconAddSelector.after(this.$addListSelector)
       this.$addListSelector.css(this.addListElemCss)
 
@@ -268,14 +392,12 @@ $(function() {
       this.$addListNameSelector = $('#tumbTag-addListName')
       this.$addListNameSelector.css(this.addListNameElemCss)
       this.$addListNameSelector.before('<p style="font-size:13px;font-weight:700px;color:#444">List name</p>')
-      this.$addListNameSelector.val('Blogging')
 
       // List tags
       this.$addListSelector.append(this.addListTagsElem)
       this.$addListTagsSelector = $('#tumbTag-addListTags')
       this.$addListTagsSelector.css(this.addListTagsElemCss)
       this.$addListTagsSelector.before('<p style="font-size:13px;font-weight:700px;color:#444">List tags</p>')
-      this.$addListTagsSelector.val('tag1\ntag2\ntag3')
 
       // Validate List
       this.$addListSelector.append(this.addListButtonElem)
@@ -284,19 +406,54 @@ $(function() {
 
       this.bindValidateList()
 
-      this.$addListSelector.show('medium')
     },
 
-    bindActions: function () {
+
+
+    isEditorVisible() {
+      return this.$addListSelector && this.$addListSelector.css('display') === 'block'
+    },
+
+
+
+    renderEditor() {
+
+      // Get the list data
+      var name
+      var list
+      if (this.editingList) {
+        name = this.editingList.name
+        list = this.editingList.list
+        this.$addListButtonSelector.text('Edit')
+      }
+      else {
+        name = 'Blogging'
+        list = ['tag1', 'tag2', 'tag3']
+        this.$addListButtonSelector.text('Done')
+      }
+
+      this.$addListNameSelector.val(name)
+      this.$addListTagsSelector.val(list.join('\n'))
+
+    },
+
+
+
+    bindActions : function () {
+
       var that = this
 
       $('#tumbTag-actions .icon_edit_pencil').click(function () {
-        that.buildNewList()
+        that.toggleAddList()
         return false
       })
+
     },
 
-    appendActions: function () {
+
+
+    appendActions : function () {
+
       this.$tumbTagSelector.append(this.actionsElem)
       this.$actionsSelector = $('#tumbTag-actions')
       this.$actionsSelector.css(this.actionsElemCss)
@@ -306,9 +463,13 @@ $(function() {
       this.$iconAddSelector.css(this.iconAddElemCss)
 
       this.bindActions()
+
     },
 
-    buildTumbTag: function () {
+
+
+    buildTumbTag : function () {
+
       if (!this.$tumbTagSelector) {
         $('body').append(this.tumbTagElem)
 
@@ -321,37 +482,55 @@ $(function() {
 
         this.appendTagLists()
       }
+
     },
 
-    showTumbTag: function () {
+
+
+    showTumbTag : function () {
+
       if (this.$tumbTagSelector && this.$tumbTagSelector.length && this.$tumbTagSelector.css('display') === 'none') {
+
         this.adjustPosition()
         // adjust position when the post's size change
         $('.post-container').bind('DOMSubtreeModified', this.adjustPosition.bind(this))
         return this.$tumbTagSelector.show()
+
       }
 
       this.buildTumbTag()
       // adjust position when the post's size change
       $('.post-container').bind('DOMSubtreeModified', this.adjustPosition.bind(this))
+
     },
 
-    hideTumbTag: function () {
+
+
+    hideTumbTag : function () {
+
       if (this.$tumbTagSelector && this.$tumbTagSelector.length) {
         this.$tumbTagSelector.hide()
       }
+
     },
 
-    periodicallyCheckIfRender: function () {
+
+
+    periodicallyCheckIfRender : function () {
+
       var that = this
 
       setTimeout(function () {
         $('.post-form--tag-editor').length ? that.showTumbTag() : that.hideTumbTag()
         that.periodicallyCheckIfRender()
       }, 2000)
+
     },
 
-    syncStorages: function () {
+
+
+    syncStorages : function () {
+
       var deferred = Q.defer(),
           tags     = JSON.parse(window.localStorage.tags)
 
@@ -362,20 +541,28 @@ $(function() {
       })
 
       return deferred.promise
+
     },
 
-    storeTagExample: function () {
-      chrome.storage.sync.set({
-        tags: [
-          {
-            name : 'Example',
-            list : ['tag1', 'tag2', 'tag3']
-          }
-        ]
-      })
+
+
+    setTagExample : function () {
+
+      this.tags = [
+        {
+          name : 'Example',
+          list : ['tag1', 'tag2', 'tag3']
+        }
+      ]
+
+      this.save()
+
     },
 
-    getSavedTags: function () {
+
+
+    getSavedTags : function () {
+
       var that     = this,
           deferred = Q.defer()
 
@@ -385,7 +572,7 @@ $(function() {
           return deferred.resolve(tags.tags)
         }
 
-        that.storeTagExample()
+        that.setTagExample()
 
         chrome.storage.sync.get('tags', function (tags) {
           return deferred.resolve(tags.tags)
@@ -393,7 +580,11 @@ $(function() {
       })
       return deferred.promise
     }
+
   }
 
+
+
   tumbTag.init()
+
 })
